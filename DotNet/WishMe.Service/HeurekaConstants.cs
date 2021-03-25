28463c4f81fead773b50414db95191d5df96ecba
Šimon Stachura <ghort@seznam.cz>
2021-03-25 15:52:12 +0100

    heureka

---
+++ DotNet/WishMe.Service/HeurekaConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace WishMe.Service
+{
+    public static class HeurekaConstants
+    {
+        public const string _GalleryThumbnailImageClassName = "c-gallery-open__thumbnail";
+        public const string _RecommendedOfferClassName = "c-offer-v3__price u-bold u-delta";
+    }
+}
--- DotNet/WishMe.Service/Startup.cs
+++ DotNet/WishMe.Service/Startup.cs
@@ -1,8 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 
 namespace WishMe.Service
 {
@@ -12,23 +10,8 @@ namespace WishMe.Service
         {
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-            });
         }
     }
 }
