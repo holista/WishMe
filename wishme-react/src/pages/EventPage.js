7ed793da9443ede02f073f0ee8beebeeaf7f069c
k-holik <holkaji@centrum.cz>
2021-06-16 16:26:14 +0200

    .

--- wishme-react/src/pages/EventPage.js
+++ wishme-react/src/pages/EventPage.js
@@ -1,9 +1,7 @@
 import { useHistory } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 
-import Carousel from "../components/carousel/Carousel";
 import NewItem from "../components/item/newItem/NewItem";
-import { uiActions } from "../store/ui-slice";
 import ItemList from "../components/item/ItemList";
 
 const EventPage = (props) => {
